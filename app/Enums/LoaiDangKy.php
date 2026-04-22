368e6798f8dfb45fb6b4828d20c602010ad7dafa
Nguyễn Vũ Hoàng Anh <nguyenhoanganh2107contact@gmail.com>
2026-04-22 13:52:33 +0700

    feat: add LoaiDangKy enum and update repomix output

---
+++ app/Enums/LoaiDangKy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum LoaiDangKy: string
+{
+    case RENTAL = 'Thuê phòng';
+    case RETURN = 'Trả phòng';
+    case CHANGE = 'Đổi phòng';
+
+    public static function values(): array
+    {
+        return array_map(static fn (self $case) => $case->value, self::cases());
+    }
+}
